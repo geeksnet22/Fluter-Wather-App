f172dbee910d87f22f9088842fa7cc8bfb4643c1
Gurinder Bhangu <gurinderbhangu@hotmail.com>
2020-06-28 14:46:00 -0600

    track user location and show corresponding weather information

--- lib/utilities/extract_data.dart
+++ lib/utilities/extract_data.dart
@@ -1,15 +1,22 @@
 import 'dart:async';
 import 'package:darksky_weather/darksky_weather_io.dart';
 import 'package:geocoder/geocoder.dart';
+import 'package:geolocator/geolocator.dart';
 
 const API_KEY = "7d7c4d51abd38384fd51a174d0771a5d";
 
 Future<List> getForecast() async {
   var darksky = new DarkSkyWeather(API_KEY, units: Units.SI);
 
-  var addresses = await Geocoder.local
-      .findAddressesFromQuery("Khanna, India");
-  String location = addresses.first.addressLine.split(',').first;
+  Position _location = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+
+
+//  var addresses = await Geocoder.local
+//      .findAddressesFromQuery("Khanna, India");
+  var addresses = await Geocoder.local.findAddressesFromCoordinates(Coordinates(_location.latitude,
+      _location.longitude));
+  String location = addresses.first.adminArea.split(',').first;
   Forecast forecast = await darksky.getForecast(
       addresses.first.coordinates.latitude,
       addresses.first.coordinates.longitude);
